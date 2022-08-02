0fce629e0780a176fcc1758889268da531b39ffc
AlexShemyakin <fax777@rambler.ru>
2022-08-02 23:54:37 +0300

    Correct date to/from excel

--- bot.py
+++ bot.py
@@ -9,11 +9,11 @@ def start_bot():
     token = '5592992190:AAHDKnVr3bEq5oDTKbslJcMQiCUygdBc9Os'
     bot = telebot.TeleBot(token)
 
-
     # активация командой start
     @bot.message_handler(commands=['start'])
     def command_hello(message):
-        bot.reply_to(message, f"Привет {message.chat.first_name}, если вы видите это сообщение, значит бот работает исправно и готов к работе.")
+        bot.reply_to(message, f"Привет {message.chat.first_name},"
+                              f" если вы видите это сообщение, значит бот работает исправно и готов к работе.")
         time.sleep(1)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.KeyboardButton("About Bot")
@@ -33,8 +33,8 @@ def start_bot():
     # Получение сообщения от user
     @bot.message_handler(content_types=['text'])
     def handle_text(message):
-        flag = 0
         global start_date
+
         if message.text == 'About Bot':
             bot.send_message(message.from_user.id, "В тестовом режиме бот анализирует olx.kz и формирует excel-файл.")
 
@@ -42,7 +42,7 @@ def start_bot():
         elif message.text == 'Start':
             bot.send_message(message.from_user.id, 'Процесс может занять несколько минут.')
             ex()
-            bot.send_document(message.from_user.id, open('test_xlsx.xlsx', 'rb'))
+            bot.send_document(message.from_user.id, open('advertisements.xlsx', 'rb'))
 
 
         elif message.text == 'News':
@@ -63,34 +63,39 @@ def start_bot():
             markup.add(btn1, btn2, btn3)
             bot.send_message(message.from_user.id, 'Процесс может занять несколько минут.'.format(message.from_user),
                              reply_markup=markup)
-            answer = f"{exnw('m')}"
-            bot.send_message(message.from_user.id, answer)
+            exnw('m')
+            bot.send_document(message.from_user.id, open('news.xlsx', 'rb'))
 
 
         elif message.text == 'За период':
-            markup = types.ReplyKeyboardRemove()
-            bot.send_message(message.from_user.id, 'введите дату начала в формате М/Д/Г'.format(message.from_user),
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btn1 = types.KeyboardButton("Назад")
+            markup.add(btn1)
+            bot.send_message(message.from_user.id, "введите дату начала в формате 'ММ.ДД.ГГ'".format(message.from_user),
                              reply_markup=markup)
-            flag += 1
 
 
-        elif message.text.isdigit() and flag == 1:
+        elif message.text.isdigit() and message.text.find('.') != -1:
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btn1 = types.KeyboardButton("Назад")
+            markup.add(btn1)
             start_date = message.text
-            bot.send_message(message.from_user.id, 'Введите конечную дату в формате М/Д/Г')
-            flag += 1
+            bot.send_message(message.from_user.id,
+                             "Введите конечную дату в формате 'ММ.ДД.ГГ'".format(message.from_user),
+                             reply_markup=markup)
 
 
-        elif message.text.isdigit() and flag == 2:
+
+        elif message.text.isdigit() and message.text.find:
             end_date = message.text
-            flag = 0
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
             btn1 = types.KeyboardButton("About Bot")
             btn2 = types.KeyboardButton("Start")
             markup.add(btn1, btn2)
             bot.send_message(message.from_user.id, 'Процесс может занять несколько минут'.format(message.from_user),
                              reply_markup=markup)
-            answer = f"{exnw('p', start_date, end_date)}"
-            bot.send_message(message.from_user.id, f'{answer}')
+            exnw('p', start_date, end_date)
+            bot.send_document(message.from_user.id, open('news.xlsx', 'rb'))
 
 
         elif message.text == 'Назад':
@@ -99,9 +104,8 @@ def start_bot():
             btn2 = types.KeyboardButton("News")
             btn3 = types.KeyboardButton("Start")
             markup.add(btn1, btn2, btn3)
-            bot.send_message(message.from_user.id, 'Вы вышли в главное меню.'.format(message.from_user), reply_markup=markup)
-
-
+            bot.send_message(message.from_user.id, 'Вы вышли в главное меню.'.format(message.from_user),
+                             reply_markup=markup)
 
     bot.polling(none_stop=True, interval=0)
 
--- main.py
+++ main.py
@@ -9,18 +9,16 @@ from webdriver_manager.firefox import GeckoDriverManager
 from bs4 import BeautifulSoup
 import re
 from operator import itemgetter
-
+import string
 
 
 def find_news(period, *args):
     list_key = ['контрабанда рогов сайгака', 'контрабанда сайгачьих рогов', 'незаконный вывоз рогов сайгака',
                 'контрабандный вывоз степных черепах', 'незаконный вывоз балобанов']
 
-    list_headers = []
-    list_date = []
-    list_href = []
-    list_keyword = []
-    count_requsts = 0
+    list_result = []
+    count_requests = 0
+    count_pages = 0
     firefox_options = Options()
     firefox_options.add_argument('user-data-dir=selenium')
     # firefox_options.add_argument("--headless")
@@ -28,8 +26,7 @@ def find_news(period, *args):
     driver.get('https://google.com/')
 
     for req in list_key:
-        list_keyword.append(req)
-        if count_requsts == 1:
+        if count_requests == 1:
             search = driver.find_element(by=By.XPATH,
                                          value='/html/body/div[4]/div[2]/form/div[1]/div[1]/div[2]/div/div[2]/input')
         else:
@@ -41,56 +38,99 @@ def find_news(period, *args):
         search.submit()
         time.sleep(1)
 
-        if period == 'm' and count_requsts == 0:
+        if period == 'm' and count_requests == 0:
             driver.find_element(by=By.CLASS_NAME, value='t2vtad').click()
             driver.find_elements(by=By.CLASS_NAME, value='KTBKoe')[1].click()
             driver.find_elements(by=By.CLASS_NAME, value='tnhqA')[12].click()
-            count_requsts = 1
+            count_requests = 1
 
-        elif period == 'p' and count_requsts == 0:
+        elif period == 'p' and count_requests == 0:
             driver.find_element(by=By.CLASS_NAME, value='t2vtad').click()
             driver.find_elements(by=By.CLASS_NAME, value='KTBKoe')[1].click()
             driver.find_elements(by=By.CLASS_NAME, value='tnhqA')[14].click()
             driver.find_element(by=By.ID, value='OouJcb').send_keys(args[0])
             driver.find_element(by=By.ID, value='rzG2be').send_keys(args[1])
             driver.find_elements(by=By.CLASS_NAME, value='fE5Rge')[0].click()
-            count_requsts = 1
+            count_requests = 1
+
+        while count_pages < 2:
+            #Поиск заголовков и ссылок
+            try:
+                search = driver.find_elements(by=By.CLASS_NAME, value='jGGQ5e')
+            except:
+                break
+            for j, i in enumerate(search):
+                list_attr = []
+                header = i.text.find('\n')
+                header = i.text[:header]
+
+                href = driver.find_element(by=By.PARTIAL_LINK_TEXT, value=header)
+                href = href.get_attribute('href')
+
+                try:
+                    date = driver.find_elements(by=By.CLASS_NAME, value='WZ8Tjf')
+                except:
+                    break
+                if re.search(r'час', f'{date[j].text}'):
+                    date_public = datetime.today().strftime('%Y.%m.%d')
+                elif re.search(r'дне', f'{date[j].text}') or re.search(r'дня', f'{date[j].text}') or\
+                        re.search(r'ден', f'{date[j].text}'):
+                    space = date[j].text.find(' ')
+                    date_public = datetime.today() - timedelta(days=int(date[j].text[:space]))
+                    date_public = date_public.strftime('%Y.%m.%d')
+                elif re.search(r'г.', f'{date[j].text}'):
+                    d_m_y = date[j].text.split(' ')
+                    day = d_m_y[0]
+                    month = d_m_y[1]
+                    year = d_m_y[2]
+                    if re.search(r'янв', month):
+                        month = '01'
+                    elif re.search(r'фев', month):
+                        month = '02'
+                    elif re.search(r'мар', month):
+                        month = '03'
+                    elif re.search(r'апр', month):
+                        month = '04'
+                    elif re.search(r'ма', month):
+                        month = '05'
+                    elif re.search(r'июн', month):
+                        month = '06'
+                    elif re.search(r'июл', month):
+                        month = '07'
+                    elif re.search(r'авг', month):
+                        month = '08'
+                    elif re.search(r'сен', month):
+                        month = '09'
+                    elif re.search(r'окт', month):
+                        month = '10'
+                    elif re.search(r'ноя', month):
+                        month = '11'
+                    elif re.search(r'дек', month):
+                        month = '12'
 
 
-        #Поиск заголовков и ссылок
-        search = driver.find_elements(by=By.CLASS_NAME, value='yuRUbf')
-        for i in search:
-            header = i.text.find('\n')
-            header = i.text[:header]
-            href = driver.find_element(by=By.PARTIAL_LINK_TEXT, value=header)
-            href = href.get_attribute('href')
-            list_href.append(href)
-            list_headers.append(header)
+                    date_public = datetime(int(year), int(month), int(day))
+                    date_public = date_public.strftime('%Y.%m.%d')
 
-        #Поиск дат
-        date = driver.find_elements(by=By.CLASS_NAME, value='WZ8Tjf')
-        for i in date:
-            if re.search(r'час', f'{i.text}'):
-                date = datetime.today().strftime('%d %B %Y г.')
-            elif re.search(r'дне', f'{i.text}') or re.search(r'дня', f'{i.text}') or re.search(r'ден', f'{i.text}'):
-                space = i.text.find(' ')
-                date = datetime.today() - timedelta(days=int(i.text[:space]))
-                date = date.strftime('%d %B %Y г.')
-            elif re.search(r'г.', f'{i.text}'):
-                date = i.text.find('г.')
-                date = i.text[:date + 2]
-            else:
-                date = 'б/д'
+                else:
+                    date_public = '0.0.0'
 
-            list_date.append(date)
+                list_attr.append(req)
+                list_attr.append(date_public)
+                list_attr.append(href)
+                list_attr.append(header)
+                list_result.append(list_attr)
+
+            try:
+                search = driver.find_element(by=By.PARTIAL_LINK_TEXT, value='Следующая')
+                count_pages += 1
+            except:
+                break
+            search.click()
 
     driver.close()
-    print(list_keyword)
-    print(list_date)
-    print(list_keyword)
-    print(list_href)
-
-    return [list_keyword, list_date, list_keyword, list_href]
+    # list_result = sorted(list_result, key=itemgetter(0, 1, 2), reverse=True)
+    return list_result
 
 
 
@@ -139,10 +179,13 @@ def main_foo():
             url_olx = f'https://www.olx.kz/list/q-{i}/?page={p}'
             page = requests.get(url_olx)
             soup = BeautifulSoup(page.text, 'html.parser')
+
             # Кол-во объявлений на каждой странице
             list_of_ads = soup.findAll('a', {"class": "thumb"})
+
             # Перебор всех объявлений со страницы и достаем от туда href
             for ad in list_of_ads:
+
                 # Если этого объявления еще нет в list_href, то добавляем
                 if ad.__getattribute__('attrs')['href'] not in list_result:
                     href = ad.__getattribute__('attrs')['href']
@@ -154,5 +197,6 @@ def main_foo():
 
 if __name__ == '__main__':
     # main_foo()
-    find_news(0, '1.1.2020', '2.2.2020')
+    find_news('p', '1.1.2020', '2.2.2020')
+    # find_news('m')
     # open_href()
